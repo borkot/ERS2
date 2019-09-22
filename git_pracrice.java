39c1826558a86d882be6c47a02e0be3190516d90
Abul Chowdhury <abul.chowdhury@csra.com>
2019-09-22 19:34:50 -0400

    added for loop

--- git_pracrice.java
+++ git_pracrice.java
@@ -4,8 +4,12 @@ public class git_pracrice {
 
 	public static void main(String[] args) {
 		
+		for(int i=0; i<30; i++){
+			
+		
 		System.out.println(" Borkot Chowdhury");
 		System.out.println(" Jamaica");
+		}
 	}
 
 }
