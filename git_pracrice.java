457cc2b63d03b18b19eb65ac580e5c283df5af8c
Abul Chowdhury <abul.chowdhury@csra.com>
2019-09-22 19:25:05 -0400

    My updated java file

---
+++ git_pracrice.java
@@ -0,0 +1,11 @@
+package Day02;
+
+public class git_pracrice {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Borkot Chowdhury");
+		System.out.println(" Jamaica");
+	}
+
+}
